3b9b9b90d63623b7d98e702645b5acdd3ef37078
eumoitinho <joao.silva489@academico.ufgd.edu.br>
2025-11-29 17:53:34 -0300

    fix: refactor promo data handling and create new type definition

---
+++ src/types/promo.ts
@@ -0,0 +1,17 @@
+// Type for promo/planilha data
+export type PlanilhaData = {
+  DESTINO: string
+  HOTEL: string
+  DATA_FORMATADA: string
+  VALOR: string
+  COM_CAFE: boolean
+  SEM_CAFE: boolean
+  MEIA_PENSAO: boolean
+  PENSAO_COMPLETA: boolean
+  ALL_INCLUSIVE: boolean
+  NUMERO_DE_NOITES: string
+  SP: boolean
+  CG: boolean
+  AEREO: boolean
+  PARCELAS?: number
+}
