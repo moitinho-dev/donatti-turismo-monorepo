ef63e79e411a7847ecfb44df50faf58e3caca2c8
eumoitinho <joao.silva489@academico.ufgd.edu.br>
2026-01-12 14:20:37 -0300

    fix: comment out Intercom initialization code in IntercomChat component

--- src/components/IntercomChat.tsx
+++ src/components/IntercomChat.tsx
@@ -4,11 +4,11 @@ import { useEffect } from 'react'
 import Intercom from '@intercom/messenger-js-sdk'
 
 export default function IntercomChat() {
-  useEffect(() => {
+  /*useEffect(() => {
     Intercom({
       app_id: 'vaca45f0',
     })
-  }, [])
+  }, [])*/ 
 
   return null
 }
